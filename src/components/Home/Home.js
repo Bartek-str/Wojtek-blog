40cfe5879751f3429597ca5dd6dac479f10215b9
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-14 17:08:06 +0200

    Config Home Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,26 @@ import React from "react";
 
 function Home() {
     return (
-        <div />
+        <div>
+            <div className='header'>
+                <div className='hamburger' />
+                <div className='menu'>
+                    <div className='aboutMe'/>
+                    <div className='contact' />
+                    <div className='logIn' />
+                </div>
+            </div>
+            <div className='empty'>
+                <div className='text' />
+                <div className='mainText' />
+            </div>
+            <div className='categories'>
+                <div className='coffee' />
+                <div className='food' />
+                <div className='tea' />
+            </div>
+            <div className='footer' />
+        </div>
     );
 }
 
