0062bd0a0d647caebc9914655c72bedd1258b218
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-14 15:22:35 +0200

    reset done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+import Home from "./components/Home";
 
 function App() {
-  return (
-      <div className="App">
-
-      </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={ Home } />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
