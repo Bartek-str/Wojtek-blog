7401c52cc3499de7539f4ba2f856cac25aedbf0c
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-15 21:55:59 +0200

    start styling categories

--- src/components/Home/Categories/Categories.js
+++ src/components/Home/Categories/Categories.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Coffee from '../../../assets/IMG_2326.jpg';
+import Tea from '../../../assets/IMG_2750.jpg'
 
 function Categories() {
     return (
         <>
             <div className='categories'>
-                <div className='coffee' />
-                <div className='food' />
-                <div className='tea' />
+                <div className='coffee'>
+                    <div>Kawa</div>
+                    <img src={ Coffee } alt='coffee' />
+                </div>
+                <div className='food'>
+                    <div>Jedzenie, wypieki...</div>
+                    <div className='foodIMG' />
+                </div>
+                <div className='tea'>
+                    <div>Herbata</div>
+                    <img src={ Tea } alt='tea' />
+                </div>
             </div>
         </>
     );
