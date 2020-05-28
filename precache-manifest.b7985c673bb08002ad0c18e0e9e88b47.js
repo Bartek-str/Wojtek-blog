daef9adac20c520447bc5cbd17495f214daa0e0a
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-28 19:20:41 +0200

    Updates

---
+++ precache-manifest.b7985c673bb08002ad0c18e0e9e88b47.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd983722e28a257947cbce2673763d9a",
+    "url": "/Wojtek-blog/index.html"
+  },
+  {
+    "revision": "c42ab827d5d0bb3ab6d6",
+    "url": "/Wojtek-blog/static/css/main.adcbf2ca.chunk.css"
+  },
+  {
+    "revision": "bc8f1d5f6db89bfb3214",
+    "url": "/Wojtek-blog/static/js/2.ad95cb9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wojtek-blog/static/js/2.ad95cb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42ab827d5d0bb3ab6d6",
+    "url": "/Wojtek-blog/static/js/main.cc4d076d.chunk.js"
+  },
+  {
+    "revision": "82fa3d11ac9d8a24e8d7",
+    "url": "/Wojtek-blog/static/js/runtime-main.0c6bd107.js"
+  },
+  {
+    "revision": "3cfcddcee695adfc8746a84e07fabbc0",
+    "url": "/Wojtek-blog/static/media/IMG_2326.3cfcddce.jpg"
+  },
+  {
+    "revision": "0561a9ded50ee554f4a4db5c2375db50",
+    "url": "/Wojtek-blog/static/media/IMG_2750.0561a9de.jpg"
+  },
+  {
+    "revision": "08fae11e97cdc30118595c36608ec516",
+    "url": "/Wojtek-blog/static/media/image0.08fae11e.jpeg"
+  },
+  {
+    "revision": "101a579d34d7a53178cda29dafdd1150",
+    "url": "/Wojtek-blog/static/media/image1.101a579d.jpeg"
+  }
+]);
