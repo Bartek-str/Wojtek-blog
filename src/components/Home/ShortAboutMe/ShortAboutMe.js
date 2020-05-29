346ba65ef28651d9a501319fd02be0cb002fae02
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-29 12:18:40 +0200

    justify aboutme

--- src/components/Home/ShortAboutMe/ShortAboutMe.js
+++ src/components/Home/ShortAboutMe/ShortAboutMe.js
@@ -5,7 +5,7 @@ function ShortAboutMe() {
         <div className='shortAboutMe'>
             <div className='img'>Miejsce na img</div>
             <div className='text'>
-                <span>Muzyk, naukowiec, humanista i kucharz. Przez takie połączenie do każdego zagadnienia podchodzę kompleksowo. Nie sztuka jest stworzyć przepis. Potrawę należy zrozumieć, poznać jej historię i społeczne powiązania. Jako naukowiec znajduję nie tylko szczegóły i&ndsp;zależności między składnikami i procesami w kuchni, ale ich podstawy działania.</span>
+                <span>Muzyk, naukowiec, humanista i kucharz. Przez takie połączenie do każdego zagadnienia podchodzę kompleksowo. Nie sztuka jest stworzyć przepis. Potrawę należy zrozumieć, poznać jej historię i społeczne powiązania. Jako naukowiec znajduję nie tylko szczegóły <br /> i zależności między składnikami i procesami w kuchni, ale ich podstawy działania.</span>
                 <span>Na tej stronie raczej nie znajdziecie raczej przepisów sensu stricte. Przedstawiam Wam historie, idee, powiązania oraz naukowe podstawy potraw i napojów z całego świata.</span>
                 <span>Krótko o sobie? Człowiek renesansu. Syn muz</span>
             </div>
